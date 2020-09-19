fe09c3620ec893b8e3af0b54aeac4b69ad2afc18
denniskbijo <dennismartin2012@gmail.com>
2020-09-19 14:07:44 +0530

    Added problem statements

--- src/test/java/com/dennis/stringcalculator/StringCalculatorTest.java
+++ src/test/java/com/dennis/stringcalculator/StringCalculatorTest.java
@@ -5,9 +5,9 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple StringCalculator.
  */
-public class AppTest 
+public class StringCalculatorTest 
     extends TestCase
 {
     /**
@@ -15,7 +15,7 @@ public class AppTest
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
+    public StringCalculatorTest( String testName )
     {
         super( testName );
     }
@@ -25,7 +25,7 @@ public class AppTest
      */
     public static Test suite()
     {
-        return new TestSuite( AppTest.class );
+        return new TestSuite( StringCalculatorTest.class );
     }
 
     /**
