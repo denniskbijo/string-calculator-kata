d5815a82901c92cdcdbfe561b5d2e9f530ce82a1
denniskbijo <dennismartin2012@gmail.com>
2020-09-19 15:05:33 +0530

    Refactored multiple return statements

--- src/main/java/com/dennis/stringcalculator/StringCalculator.java
+++ src/main/java/com/dennis/stringcalculator/StringCalculator.java
@@ -41,19 +41,19 @@ if there are multiple negatives, show all of them in the exception message.
 public class StringCalculator {
 
 	public static int Add(String numbers) {
+		int sum = 0;
 
 		if (numbers.isEmpty()) {
-			return 0;
+			sum = 0;
 		} else if (numbers.contains(",")) {
 			// Split by comma and add numbers
-			int sum = 0;
 			String[] split = numbers.split(",");
 			for (int i = 0; i < split.length; i++) {
 				sum += Integer.parseInt(split[i]);
 			}
-			return sum;
 		} else {
-			return Integer.parseInt(numbers);
+			sum = Integer.parseInt(numbers);
 		}
+		return sum;
 	}
 }
